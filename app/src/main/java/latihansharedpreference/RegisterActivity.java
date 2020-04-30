910cd39f5f8b5018c3813dca191959489d101477
Chaerrull <62976741+Chaerrull@users.noreply.github.com>
2020-05-01 06:31:42 +0700

    Update RegisterActivity.java

--- app/src/main/java/latihansharedpreference/RegisterActivity.java
+++ app/src/main/java/latihansharedpreference/RegisterActivity.java
@@ -58,46 +58,15 @@ public class RegisterActivity extends AppCompatActivity {
         edtUserName.setError(null);
         edtPassWord.setError(null);
         edtRePassWord.setError(null);
-        View fokus = null;
-        boolean cancel = false;
 
-        //Set Input Value dari View
+
+
         String userName = edtUserName.getText().toString();
         String password = edtPassWord.getText().toString();
         String rePassword = edtRePassWord.getText().toString();
         String phoneNumber = edtPhoneNumber.getText().toString();
 
 
-        // Jika form user kosong atau memenuhi kriteria di Method cekUser() maka, Set error di Form-
-        // User dengan menset variable fokus dan error di Viewnya juga cancel menjadi true*/
-        if (TextUtils.isEmpty(userName)){
-            edtUserName.setError("Harus diisi");
-            fokus = edtUserName;
-            cancel = true;
-        }else if(cekUser(userName)){
-            edtUserName.setError("Username sudah terdaftar");
-            fokus = edtUserName;
-            cancel = true;
-        }
-
-        // Jika form password kosong dan memenuhi kriteria di Method cekPassword maka,
-        // Reaksinya sama dengan percabangan User di atas. Bedanya untuk Password dan Repassword*/
-        if (TextUtils.isEmpty(password)){
-            edtPassWord.setError("Harus Diisi");
-            fokus = edtPassWord;
-            cancel = true;
-        }else if (!cekPassword(password,rePassword)){
-            edtPassWord.setError("Password yang dimasukkan tidak sesuai");
-            fokus = edtPassWord;
-            cancel = true;
-        }
-
-        // Jika cancel true, variable fokus mendapatkan fokus. Jika false, maka
-        // Kembali ke LoginActivity dan Set User dan Password untuk data yang terdaftar */
-        if (cancel){
-            fokus.requestFocus();
-        }else{
-// Deklarasi Model
             UserModel userModel = new UserModel();
             userModel.setUsername(userName);
             userModel.setPassword(password);
@@ -109,18 +78,4 @@ public class RegisterActivity extends AppCompatActivity {
             startActivity(new Intent(getBaseContext(), HomeActivity.class));
             finish();
         }
-
     }
-
-
-    // True jika parameter user sama dengan data user yang terdaftar dari Preferences */
-    private boolean cekUser(String user){
-        return user.equals(Preferences.getRegisteredUser(getBaseContext()));
-    }
-
-    // True jika parameter password sama dengan parameter repassword */
-    private boolean cekPassword(String password, String repassword){
-        return password.equals(repassword);
-    }
-
-}
